c6a1433c157a49ac04a360a58297ae20c893f9c4
sze <sze@Sallys-MacBook-Pro-2.local>
2021-07-01 15:22:46 -0400

    Remove db.js

--- src/db.js
+++
@@ -1,9 +0,0 @@
-import Airtable from 'airtable';
-import { AIRTABLE_API_KEY } from './secrets';
-
-// export const base = require('airtable').base(AIRTABLE_API_KEY);
-
-// export const activityAll = base('Activity').select({
-//     maxRecords: 12,
-//     view: "All"
-// })
